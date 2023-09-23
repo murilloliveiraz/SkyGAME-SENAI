0d53c0c27253c6964c4a6c97db756317679af833
Murillo Oliveira <murillo1047@gmail.com>
2023-09-22 21:19:27 -0300

    feat: 'comments'

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,9 +9,11 @@ const personagem = document.querySelector(".plane");
 const villain = document.querySelector('.vilao')
 const game = document.querySelector(".game")
 
+//monitora o click no botao start e inicia o jogo
 startButton.addEventListener('click', ()=> {
     startButton.style.display = 'none'
     startedGame.style.display = 'flex'
+    //faz o vilao atirar a cada 2.5s
     setInterval(atirarVilao, 2500)
 })
 
@@ -19,9 +21,9 @@ const imagem1URL = "./assets/characters/free_plane_sprite/png/Plane/Fly (1).png"
 const imagem2URL = "./assets/characters/free_plane_sprite/png/Plane/Fly (2).png";
 const tiroImg = './assets/characters/free_plane_sprite/png/Bullet/Bullet (1).png'
 const tiroVilao = './assets/characters/plane_pack/planes/torpedo/torpedo_black.png'
-
 let imagemAtual = 1;
 
+//animacao do personagem
 function trocarImagem() {
   if (imagemAtual === 1) {
     personagem.src = imagem2URL;
@@ -34,32 +36,36 @@ function trocarImagem() {
   }
 }
 
-// Define o intervalo para trocar a imagem a cada 2 segundos (2000 milissegundos)
+// Define o intervalo para trocar a imagem do personagem a cada 2 segundos (2000 milissegundos)
 setInterval(trocarImagem, 500);
 
+//posicao que é deslocada a cada movimento do personagem
 const step = 30;
+//posicao que é deslocada a cada movimento do vilao
 const villainStep = 30;
+//posicao inicial
 let posicaoVertical = 0;
 
+//muda a posicao do personagem
 function updatePersonagemPosition() {
     const minY = 0;
     const maxY = game.clientHeight - personagem.clientHeight;
-    const elementoRect = personagem.getBoundingClientRect();
-    const janelaHeight = window.innerHeight;
 
+    //limita de passar da borda bottom
     if (posicaoVertical > 400) {
-      console.log(posicaoVertical)
-        posicaoVertical = 390;
-        // personagem.style.top = 390 + 'px';
+      posicaoVertical = 390;
     }
-
+    
+    //limita de passar da borda top
     if(posicaoVertical < -400) {
       posicaoVertical = -390
     }
     
+  //atualiza a posicao do versonagem
   personagem.style.top = posicaoVertical + 'px';
 }
 
+//monitora o evento de pressionamento de teclas
 document.addEventListener('keydown', (event) => {
   switch (event.key) {
     case 'ArrowUp':
@@ -69,11 +75,11 @@ document.addEventListener('keydown', (event) => {
       posicaoVertical += step;
       break;
   }
-  // limitarColisao(personagem)
+  // atualiza a posicao do elemento com base na tecla que foi apertada
   updatePersonagemPosition();
 });
 
-//ATIRAR ====== INICIO
+//Função para atirar
 function atirar() {
     const tiro = document.createElement('img');
     tiro.src = tiroImg;
@@ -82,21 +88,32 @@ function atirar() {
     tiro.id = 'tiro'; 
     document.body.appendChild(tiro);
   
+    //captura a posicao do personagem
     const personagemRect = personagem.getBoundingClientRect();
+
+    //atira com base na posicao do personagem
     tiro.style.left = (personagemRect.left + personagemRect.width / 2) + 'px';
     tiro.style.top = (personagemRect.top + personagemRect.height / 2) + 'px';
   
+
+    // Cria um intervalo que move o elemento 'tiro' para a direita a cada 10 ms
     const tiroInterval = setInterval(() => {
-      const tiroRect = tiro.getBoundingClientRect();
-      if (tiroRect.right < window.innerWidth) {
-        tiro.style.left = (parseInt(tiro.style.left) || 0) + 10 + 'px';
-      } else {
-        clearInterval(tiroInterval);
-        document.body.removeChild(tiro);
-      }
+        // Obtém as coordenadas e dimensões do elemento 'tiro'
+        const tiroRect = tiro.getBoundingClientRect();
+        
+        // Verifica se 'tiro' não alcançou a borda direita da tela
+        if (tiroRect.right < window.innerWidth) {
+          // Move 'tiro' 10 pixels para a direita
+          tiro.style.left = (parseInt(tiro.style.left) || 0) + 10 + 'px';
+        } else {
+          // Remove 'tiro' e cancela o intervalo quando alcança a borda direita
+          clearInterval(tiroInterval);
+          document.body.removeChild(tiro);
+        }
     }, 10);
   }
 
+  //funcao que faz o vilao atirar
   function atirarVilao() {
     const tiro = document.createElement('img');
     tiro.src = tiroVilao;
@@ -105,28 +122,39 @@ function atirar() {
     tiro.id = 'tiro'; 
     document.body.appendChild(tiro);
   
+    // Obtém as coordenadas e dimensões do elemento 'tiro'
     const personagemRect = villain.getBoundingClientRect();
+
+    //atira com base na posicao do vilao
     tiro.style.left = (personagemRect.left + personagemRect.width / 2) + 'px'; // Use 'left' em vez de 'right'
     tiro.style.top = (personagemRect.top + personagemRect.height / 2) + 'px';
   
+    // Cria um intervalo que move o elemento 'tiro' para a esquerda a cada 10 ms
     const tiroInterval = setInterval(() => {
-      const tiroRect = tiro.getBoundingClientRect();
-      if (tiroRect.left > 0) { // Alterado o limite para quando o tiro deve ser removido
-        tiro.style.left = (parseInt(tiro.style.left) || 0) - 5 + 'px'; // Subtrai pixels para mover da direita para a esquerda
-      } else {
-        clearInterval(tiroInterval);
-        document.body.removeChild(tiro);
-      }
-    }, 10);
+        // Obtém as coordenadas e dimensões do elemento 'tiro'
+        const tiroRect = tiro.getBoundingClientRect();
+        
+        // Verifica se 'tiro' não alcançou a borda esquerda da tela
+        if (tiroRect.left > 0) {
+          // Move 'tiro' 5 pixels para a esquerda
+          tiro.style.left = (parseInt(tiro.style.left) || 0) - 5 + 'px';
+        } else {
+          // Remove 'tiro' e cancela o intervalo quando alcança a borda esquerda
+          clearInterval(tiroInterval);
+          document.body.removeChild(tiro);
+        }
+      }, 10);
   }
-  
-//ATIRAR ====== FIM
 
+// Adiciona um ouvinte de eventos ao documento que detecta pressionamentos de tecla
 document.addEventListener('keydown', (e) => {
-    if (e.key === ' ') {
-        atirar();
-    }
+  // Verifica se a tecla pressionada é a barra de espaço
+  if (e.key === ' ') {
+      // Chama a função 'atirar()' quando a barra de espaço é pressionada
+      atirar();
+  }
 });
+
 
 
   
